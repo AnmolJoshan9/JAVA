c1f01da32a38f09d467fbd91afc8c26c8fd04b9e
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-01 23:17:54 +0530

    Increment Decrement ops

---
+++ Operators/Arithetic ops/src/incrementDecrement.java
@@ -0,0 +1,37 @@
+import  java.util.*;
+
+public class incrementDecrement {
+    public static void main(String[] args) {
+
+
+//        int x = 5;
+//        x++;
+//        ++x;
+//        System.out.println(x);
+
+//        int x = 5;
+//        int y;
+//        y = x++;
+//        y = ++x;
+//        System.out.println(x + " " + y);
+
+//        int x = 5 , y = 4 , z;
+////        z = 2*x++ + 3 * ++y;
+//        z = 2*x++ + 3 * ++x;
+//        System.out.println(z);
+
+//        float a = 3.5f;
+//        a++;
+//        System.out.println(a);
+
+        char c = 'A';
+        c++;
+        System.out.println(c);
+
+//        byte b = 5;
+////        b++;
+////        b = b+1;
+//        System.out.println(b);
+
+    }
+}
