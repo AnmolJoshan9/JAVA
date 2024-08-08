688a04c51efc76a9dbce5b6d0f4eb75ddfe12bc9
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-09 00:12:44 +0530

    Printing

---
+++ Printing/src/Printing.java
@@ -0,0 +1,14 @@
+
+public class Printing {
+    public static void main(String[] args) {
+
+        int x= 10 , y = 20;
+        float b = 12.55f;
+        char c = 'A';
+        String str = "Hello";
+        System.out.println(str);
+        System.out.println(x+y);
+        System.out.println("sum is " + (x + y));
+        System.out.println("Sum of " + x + " and " + y + " is " + (x+y));
+    }
+}
