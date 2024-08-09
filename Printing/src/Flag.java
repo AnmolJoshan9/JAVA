6926473ee527fd3c2c7ec1d6dcbc3a64a83c2e6a
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-09 23:33:37 +0530

    Flag

---
+++ Printing/src/Flag.java
@@ -0,0 +1,23 @@
+
+public class Flag {
+    public static void main(String[] args) {
+
+//        int a = -10;
+        int a = 10;
+        float f = 123.45f;
+        String str = "Java programming";
+
+
+//        System.out.printf("%d" , a);
+//        System.out.printf("%5d" , a);
+//        System.out.printf("%05d" , a);
+//        System.out.printf("%(5d" , a);
+//        System.out.printf("%+5d" , a);
+//        System.out.printf("%f" , f);
+//        System.out.printf("%6f" , f);
+//        System.out.printf("%6.2f" , f);
+//        System.out.printf("%20s" , str);
+        System.out.printf("%-20s" , str);
+
+    }
+}
