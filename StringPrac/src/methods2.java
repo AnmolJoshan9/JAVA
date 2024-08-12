1d773c8479d2fbe315429176eabff6da62f9b102
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-12 23:52:19 +0530

    methods2

---
+++ StringPrac/src/methods2.java
@@ -0,0 +1,33 @@
+
+public class methods2 {
+    public static void main(String[] args) {
+
+//        String str1 = "Mr. Shahrukh Khan";
+        String str2 = "www.udemy.co.in";
+
+//        System.out.println(str1.startsWith("Mr."));
+//        System.out.println(str1.startsWith("mr."));
+//        System.out.println(str1.startsWith("Shah" , 4));
+
+//        System.out.println(str1.endsWith("Khan"));
+//        System.out.println(str1.endsWith("khan"));
+
+//        for (int i = 0; i < str1.length(); i++) {
+//            System.out.println(str1.charAt(i));
+//        }
+
+        System.out.println(str2.indexOf('.'));
+        System.out.println(str2.indexOf("udemy"));
+        System.out.println(str2.indexOf("." , 4));
+        System.out.println(str2.lastIndexOf('.'));
+
+
+
+
+
+
+
+
+
+    }
+}
