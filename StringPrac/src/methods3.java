3868c62319772bf00de73dcbd19ed91f9084b89e
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-13 00:45:28 +0530

    Methods3

---
+++ StringPrac/src/methods3.java
@@ -0,0 +1,29 @@
+
+public class methods3 {
+    public static void main(String[] args) {
+
+//        String str1 = "Pyramid";
+//        String str1 = "pyramid";
+        String str1 = "the great wall";
+        String str4 = " of china";
+
+//        String str2 = "Pyramid";
+//        String str2 = "Pyramid";
+
+//        String str3 = new String("Pyramid");
+//        String str3 = new String("china wall");
+
+
+
+//        System.out.println(str1 == str3);
+//        System.out.println(str1.equals(str3));
+//        System.out.println(str1.equalsIgnoreCase(str3));
+
+//        System.out.println(str1.compareTo(str2));
+
+        System.out.println(str1.contains("wall"));
+        System.out.println(str1.concat(str4));
+        System.out.println(str1+str4);
+    }
+
+}
