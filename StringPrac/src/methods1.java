85683b57d19009c06cbf5ebd5b8c51f3bd6c2058
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-12 23:14:00 +0530

    Methods1

---
+++ StringPrac/src/methods1.java
@@ -0,0 +1,34 @@
+public class methods1 {
+    public static void main(String[] args) {
+
+//        String str = new String("netbeans");
+        String str = new String("netbeans");
+
+//        int len = str.length();
+//        System.out.println(len);
+//        System.out.println(str.length());
+
+//        str = str.toUpperCase();
+//        System.out.println(str);
+
+//        String str1 = str.toUpperCase();
+//        System.out.println(str1);
+//        System.out.println(str.toUpperCase());
+
+//        System.out.println(str);
+//
+//        str = str.trim();
+//        System.out.println(str);
+//
+//        String str2 = str.trim();
+//        System.out.println(str2) ;
+
+//        String str3 = str.substring(3);
+//        String str3 = str.substring(3 , 5);
+//        System.out.println(str3);
+
+        String str4 = str.replace('e' , 'm');
+        System.out.println(str4 + " "+ str);
+
+    }
+}
