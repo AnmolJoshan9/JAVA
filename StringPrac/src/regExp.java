e63daad2d519dcb18578460c6429e8ae7dd88131
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-13 22:57:22 +0530

    Regular Expressions

---
+++ StringPrac/src/regExp.java
@@ -0,0 +1,31 @@
+
+public class regExp {
+    public static void main(String[] args) {
+
+//        String str1 = "f";
+//        String str1 = "8";
+//        String str1 = "%";
+//        String str2 = "abc";
+//        String str2 = "b";
+//        String str2 = "ab";
+//        String str2 = "p";
+//        String str2 = "a7";
+//        String str2 = "k8";
+//        String str3 = "k";
+//        String str3 = "c";
+//        String str3 = "bca";
+        String str3 = "abc";
+
+//        System.out.println(str1.matches("."));
+//        System.out.println(str2.matches("."));
+//        System.out.println(str2.matches("[abc]"));
+//        System.out.println(str2.matches("[^abc]"));
+//        System.out.println(str2.matches("[a-z]"));
+//        System.out.println(str2.matches("[a-z0-9]"));
+//        System.out.println(str2.matches("[a-z][0-9]"));
+//        System.out.println(str3.matches("a|b"));
+        System.out.println(str3.matches("abc"));
+
+
+    }
+}
