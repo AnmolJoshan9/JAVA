93f69814a3fa9c0f5cac85e4db01e42257505767
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-13 23:04:04 +0530

    Meta Characters part of REGEXP

---
+++ StringPrac/src/metaCharacters.java
@@ -0,0 +1,20 @@
+
+public class metaCharacters {
+    public static void main(String[] args) {
+
+//        String str1 = "a";
+//        String str1 = "5";
+//        String str1 = "$";
+        String str1 = " ";
+
+
+
+//        System.out.println(str1.matches("\\w"));
+//        System.out.println(str1.matches("\\d"));
+//        System.out.println(str1.matches("\\D"));
+//        System.out.println(str1.matches("\\W"));
+//        System.out.println(str1.matches("\\s"));
+        System.out.println(str1.matches("\\S"));
+
+    }
+}
