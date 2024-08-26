339421c27f8be1ae75e2b450a96cc569bce5fcbc
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-26 22:56:56 +0530

    Practice Switch

---
+++ Relational/src/practiceSwitch.java
@@ -0,0 +1,52 @@
+
+
+public class practiceSwitch {
+    public static void main(String[] args) {
+
+//        int n = 3;
+
+        String str = "1";
+
+//        Comparing With IF ELSE Condition
+
+//        if(n == 1){
+//            System.out.println("one");
+//        } else if (n == 2) {
+//            System.out.println("Two");
+//        } else if (n == 3) {
+//            System.out.println("Three");
+//        }else {
+//            System.out.println("Not a Valid Number");
+//        }
+
+//        switch (n){
+//            case 1: System.out.println("one");
+//                break;
+//            case 2:
+//                System.out.println("Two");
+//                break;
+//            case 3:
+//                System.out.println("Three");
+//                break;
+//            default:
+//                System.out.println("Invalid");
+//        }
+
+        switch (str){
+            case "1":
+                System.out.println("one");
+                break;
+            case "2":
+                System.out.println("Two");
+                break;
+            case "3":
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+
+
+
+    }
+}
