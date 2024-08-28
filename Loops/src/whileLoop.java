3695e90034efd035aec590642737f65dd0cd15d5
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-08-28 23:16:48 +0530

    While Loop

---
+++ Loops/src/whileLoop.java
@@ -0,0 +1,40 @@
+
+public class whileLoop {
+    public static void main(String[] args) {
+
+//        int i = 1;
+
+//        ----While Loop
+
+//        int i = 100;
+//        while(i < 100){
+//            System.out.println(i);
+//            i = i*2;
+//        }
+
+//        while(i < 100){
+//            System.out.println(i);
+//            i = i*2;
+//        }
+
+        int x = 1;
+        while (true){
+            System.out.println(x);
+            x++;
+        }
+
+//        ---do while loop
+
+//        do{
+//            System.out.println(i);
+//            i = i*2;
+//        }while (i < 100);
+
+
+//        do{
+//            System.out.println(i);
+//            i = i*2;
+//        }while (i < 100);
+
+    }
+}
