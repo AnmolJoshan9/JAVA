3a079d996dc381d4c60a69fd9cd4ff47c6de7e29
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-10-09 21:05:50 +0530

    multiplication Table

---
+++ Loops/src/multiplicationTable.java
@@ -0,0 +1,14 @@
+
+
+public class multiplicationTable {
+    public static void main(String[] args) {
+
+//        want to do multiplication here like 5*1= 5
+
+        int n = 5;
+        for(int i = 1; i<=10 ;i++)
+        {
+            System.out.println(n + "x" + i + "=" + n * i );
+        }
+    }
+}
