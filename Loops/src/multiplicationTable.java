9c1fcb97fa9172a160a077ae9d0d21e80b22efcd
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-10-09 21:09:34 +0530

    multiplication Table

--- Loops/src/multiplicationTable.java
+++ Loops/src/multiplicationTable.java
@@ -1,11 +1,15 @@
-
+import java.util.Scanner;
 
 public class multiplicationTable {
     public static void main(String[] args) {
 
 //        want to do multiplication here like 5*1= 5
 
-        int n = 5;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
         for(int i = 1; i<=10 ;i++)
         {
             System.out.println(n + "x" + i + "=" + n * i );
