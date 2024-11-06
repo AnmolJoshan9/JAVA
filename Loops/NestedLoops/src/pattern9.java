cba86291596d7c3d7a4d9dc5b6fd1b21b8025cea
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-11-06 23:23:33 +0530

    pattern 9

--- Loops/NestedLoops/src/pattern9.java
+++ Loops/NestedLoops/src/pattern9.java
@@ -1,5 +1,16 @@
 public class pattern9 {
     public static void main(String[] args) {
 
+        for(int i = 1 ; i <= 5 ;i++){
+            for(int j = 1 ; j <= 5 ;j++){
+                if(i <= j){
+                    System.out.print(("* "));
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
