9f901f35d27ddaad0b0902fcf4eebe8ada0de4fc
Anmolpreet Singh <anmolpreet1855@gmail.com>
2024-11-12 22:38:53 +0530

    Array Practice

---
+++ Arrays/src/arrayPractice.java
@@ -0,0 +1,33 @@
+
+public class arrayPractice {
+    public static void main(String[] args) {
+
+        int A[] = new int[10];
+
+        int B[] = {1,2,3,4,5};
+        B[2] = 15;
+
+        System.out.println(B.length);
+
+
+//        int[] D = new int[5];
+
+//        for Loop is using there
+//        for(int i=0 ;i<A.length;i++){
+//            System.out.println(A[i]);
+//        }
+//        for(int i=0 ;i<B.length;i++){
+//            System.out.println(B[i]);
+//        }
+
+//        for(int i=0 ;i<B.length ; i++){
+//            System.out.println(B[i]++);
+//        }
+//        now i have to use for each loop here
+
+//        for(int x : B){
+//            System.out.println(x);
+////            System.out.println(x++);
+//        }
+    }
+}
